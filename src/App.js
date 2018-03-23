0aa7526b7779699f83a44b370ee9db5d905ca6d8
Alison Lui <Alison@Alisons-MacBook-Pro-2.local>
2018-03-23 13:51:59 -0400

    database connection working

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor() {
+        super();
+        this.state = {
+            boxers: {}
+        };
+    }
+
+    simpleQuery() {
+        fetch("https://okp1u501a5.execute-api.us-east-2.amazonaws.com/test/boxers")
+        .then(results => {
+            return results.json();
+        }).then(data => {
+            this.setState({boxers: data});
+            console.log("state", this.state.boxers);
+        })
+    }
+
+    render() {
+        var headings = [];
+        var values = [];
+        for (var key in this.state.boxers) {
+            headings.push(<th key={key}>{key}</th>);
+            values.push(<td key={key}>{this.state.boxers[key]}</td>);
+        }
+        return (
+            <div className="App">
+            <header className="App-header">
+            <h1 className="App-title">Welcome to MatchBox</h1>
+            </header>
+            <p className="App-intro">
+                Click the button to query the database.
+            </p>
+            <p>
+                <button onClick={() => this.simpleQuery()}>select * from boxers</button>
+            </p>
+            <table>
+                <tbody>
+                    <tr>
+                        {headings}
+                    </tr>
+                    <tr>
+                        {values}
+                    </tr>
+                </tbody>
+            </table>
+            </div>
+        );
+    }
 }
 
 export default App;
