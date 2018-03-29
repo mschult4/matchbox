2e4819381eeeca5d21c898ff5817f516ce1b3fbc
Madalyn Giovanni Schulte <mschult4@nd.edu>
2018-03-29 01:12:55 -0400

    hacky

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
@@ -8,6 +7,7 @@ class App extends Component {
         super();
         this.state = {
             "boxers": {},
+			"spars" : {},
 			"year": "",
 			"first": "",
 			"last": "",
@@ -50,6 +50,24 @@ class App extends Component {
 			"captain_d" : "",
 			"gender_d" : "",
 			"boxer_id_d" : "",
+
+			"num_spar" : "",
+			"date" : "",
+			"boxer_id" : "",
+			"boxer_first" : "",
+			"boxer_last" : "",
+			"opp_id" : "",
+			"opp_first" : "",
+			"opp_last" : "",
+			"num_rounds" : "",
+			"len_rds" : "",
+			"commments" : "",
+			"score" : "",
+			"coach_initials" : "",
+			"season" : "",
+			"spars.gender" : "",
+			"med_comm" : "",
+			"page" : "boxers",
 			//"display" : "Pending",
 			//"username" : "password"
         };
@@ -239,7 +257,8 @@ class App extends Component {
 		
 	}
 
-    render() {
+
+	ren_boxers() {
         let headings = ["boxer_id", "mixed_first","mixed_last","mixed_goes_by","year","hall","eligible","experience","vet_years","weight","handedness","captain","gender"];
         let heading_disp = ["Boxer ID", "First Name", "Last Name", "Nickname", "Year", "Hall", "Eligible", "Experience", "Vet Years", "Weight", "Handedness", "Captain", "Gender"];
         var rows = [];
@@ -267,10 +286,7 @@ class App extends Component {
             <p>
                 <button onClick={() => this.starQuery()}>select * from boxers</button>
             </p>
-	    <Link to="/spars">Spars</Link><br />
-	    <Link to="/signups">Signups</Link><br />
-	    <Link to="/bracket">Bracket</Link><br />
-	    <Link to="/stats">Stats</Link><br />
+	    <button onClick={() => {this.setState({"page" : "spars"})}} >SPARS</button> <br />
 			<form>
 				Query<br/>
 				<label>
@@ -539,7 +555,8 @@ class App extends Component {
 
 				<button type="button" onClick={() => this.delete_func()}>Submit</button>
 			</form>
-		
+	
+
 
 	
 			<br />
@@ -576,6 +593,137 @@ class App extends Component {
 		);
 		} */
     }
+
+	
+
+    sparQuery() {
+		console.log("top of simple: ", this.state.spars);	
+		var querylist = [];
+		var bad_keys = ["num_spar_i", "date_i", "boxer_id_i", "opp_id_i", "num_rounds_i", "len_rds_i", "commments_i", "score_i", "coach_initials_i", "season_i", "gender_i", "med_comm_i", "num_spar_u", "date_u", "boxer_id_u", "opp_id_u", "num_rounds_u", "len_rds_u", "commments_u", "score_u", "coach_initials_u", "season_u", "gender_u", "med_comm_u", "num_spar_d", "date_d", "boxer_id_d", "opp_id_d"]
+		for (var key in this.state) {
+			console.log("key: ", key);
+			console.log("value: ", this.state[key]);
+			if (key !== "spars" && this.state[key] !== "" && !bad_keys.includes(key)) {
+				var querystr = key;
+				querystr += "=";
+				querystr += this.state[key];
+				querylist.push(querystr);
+				console.log(querylist);
+			}
+		}
+
+		var URL="https://okp1u501a5.execute-api.us-east-2.amazonaws.com/test/spars";
+
+		var middle = "";
+		console.log(querylist);
+		if (querylist.length !== 0) { //pretty later
+			middle = "?"
+			middle += querylist.join("&");
+			console.log(middle);
+		}
+				
+        fetch(URL+middle)
+        .then(results => {
+            return results.json();
+        }).then(data => {
+
+            this.setState({"spars": data});
+            console.log("SPARZZZZ", this.state.spars);
+        })
+    }
+
+
+	ren_spars() {
+
+        let headings = ["num_spar", "date", "boxer_first" , "boxer_last", "opp_first", "opp_last", "num_rounds", "len_rds", "comments", "score", "coach_initials", "season", "gender", "med_comm"];
+        let heading_disp = ["Spar Number", "Date", "Boxer First" , "Boxer Last","Opponent First", "Opponent Last", "Num Rounds", "Length Rounds", "Comments", "Score", "Coach", "Season", "Gender", "Medical Comments"];
+        var rows = [];
+        rows.push(heading_disp.map((str) => <th key={str}>{str}</th>));
+        for (var row_num = 0; row_num < this.state.spars.length; row_num++) {
+            var values = [];
+
+            for (var i=0; i < headings.length; i++) {
+               		values.push(<td key={i}>{this.state.spars[row_num][headings[i]]}</td>);
+            	}
+            rows.push(<tr key={row_num}>{values}</tr>);
+        }
+
+		
+		return(<div className="App">
+			<header className="App-header">
+            <h1 className="App-title">Welcome to MatchBox</h1>
+            </header>
+			<h2>Spars</h2>
+			<br />
+	    <button onClick={() => {this.setState({"page" : "boxers"})}} >BOXERS</button> <br />
+			<br />
+			<form>
+				Query<br/>
+				<label>
+				Num Spar:
+				<input type="text" name="num_spar" onChange={(evt) => this.make_query(evt)}/>
+				</label>
+				{ /*<label>
+				Date:
+				<input type="text" name="date" onChange={(evt) => this.make_query(evt)}/>
+				</label> */ } 
+				<label>
+				Boxer ID:
+				<input type="text" name="boxer_id" onChange={(evt) => this.make_query(evt)}/>
+				</label>
+				<label>
+				Boxer First:
+				<input type="text" name="boxer_first" onChange={(evt) => this.make_query(evt)}/>
+				</label>
+				<label>
+				Boxer Last:
+				<input type="text" name="boxer_last" onChange={(evt) => this.make_query(evt)}/>
+				</label>
+				<label>
+				Opponent ID:
+				<input type="text" name="opp_id" onChange={(evt) => this.make_query(evt)}/>
+				</label>
+				<label>
+				Opponent First:
+				<input type="text" name="opp_first" onChange={(evt) => this.make_query(evt)}/>
+				</label>
+				<label>
+				Opponent Last:
+				<input type="text" name="opp_last" onChange={(evt) => this.make_query(evt)}/>
+				</label>
+				<label>
+				Score:
+				<input type="text" name="score" onChange={(evt) => this.make_query(evt)}/>
+				</label>
+
+				<button type="button" onClick={() => this.sparQuery()}>Submit</button>
+			</form>
+
+	
+	
+			<br />
+            <table>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
+            </div>
+
+	
+
+		);
+	}
+
+
+	render() {
+		if (this.state["page"] === "boxers") {
+			return this.ren_boxers();
+		} else if (this.state["page"] === "spars") {
+			return this.ren_spars();
+		}
+
+	}
+
 }
 
 export default App;
