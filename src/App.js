c92d36af0f2058d68bd8677e99a46dafc68dee37
Alison Lui <Alison@Alisons-MacBook-Pro-2.local>
2018-04-09 21:51:43 -0400

    navigation bar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Boxers from './Boxers';
 import Spars from './Spars';
 import Login from './Login';
+import NavigationBar from './NavigationBar';
 
 class App extends Component {
 
@@ -33,9 +34,26 @@ class App extends Component {
 		if (this.state["page"] === "login") {
 			return (<Login changePage={this.change_page}/>);
         } else if (this.state["page"] === "boxers") {
-			return (<Boxers changePage={this.change_page}/>);
+			return (
+                <div>
+                    <NavigationBar changePage={this.change_page}/>
+                    <Boxers changePage={this.change_page}/>
+                </div>
+            );
 		} else if (this.state["page"] === "spars") {
-			return (<Spars changePage={this.change_page}/>);
+			return (
+                <div>
+                    <NavigationBar changePage={this.change_page}/>
+                    <Spars changePage={this.change_page}/>
+                </div>
+            );
+        } else if (this.state["page"] === "brackets") {
+            return (
+                <div>
+                    <NavigationBar changePage={this.change_page}/>
+                    <Brackets changePage={this.change_page}/>
+                </div>
+            );
 		} else {
             console.log("page", this.state["page"]);
             return (<h2>Sorry, page not found</h2>);
--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import './Navigationbar.css';
 
 class NavigationBar extends Component {
 
@@ -10,7 +9,16 @@ class NavigationBar extends Component {
         };
     }
 
-	render() {
+    boxer_options() {
+        return (
+            <ul>
+                <li>My Profile</li>
+                <li>Spar Signups</li>
+            </ul>
+        );
+    }
+
+    captain_options() {
         return (
             <ul>
               <li>My Profile</li>
@@ -20,9 +28,35 @@ class NavigationBar extends Component {
               <li>Brackets</li>
             </ul>
         );
+    }
 
+    coach_options() {
+        return (
+            <ul>
+              <li>Boxer Data</li>
+              <li>Spar Data</li>
+              <li onClick(() => this.props.changePage("brackets"))>Brackets</li>
+              <li>Spar Schedule</li>
+            </ul>
+        );
+    }
+
+	render() {
+        if (sessionStorage.usertype) {
+            if (sessionStorage.usertype === "coach") {
+                return this.coach_options();
+            } else if (sessionStorage.usertype === "captain") {
+                return this.captain_options();
+            } else if (sessionStorage.usertype === "boxer") {
+                return this.boxer_options();
+            } else {
+                return (<p>User type error</p>);
+            }
+        } else {
+            return (<p>Session storage error</p>);
+        }
 	}
 
 }
 
-export default App;
+export default NavigationBar;
