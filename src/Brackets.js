e9b20f7e62089af354fcec1d2be38e0b2171e4f2
Madalyn Giovanni Schulte <mschult4@nd.edu>
2018-04-11 21:45:33 -0400

    prints boxers ranked by av_score and num_spars

--- src/Brackets.js
+++ src/Brackets.js
@@ -25,18 +25,30 @@ class Brackets extends Component {
 	}
 
 	render() {
+		console.log("top of render: ", this.state.rankings);
 		let headings = ["boxer_id", "first", "last", "weight", "score", "av_score", "num_spars", "num_scored"];
 
 		var rows = [];
 		var values = [];
 		rows.push(headings.map((str) => <th key={str}>{str}</th>));
 
-		for (var row_num = 0; row_num < this.state.rankings.length; row_num++) {
+		console.log("length: ", this.state.rankings.length);
+		var row_num = 0;
+		for (var key in this.state.rankings) {
+			values = [];
+			console.log("key", key);
+			console.log(this.state.rankings[key]);
+			//values.push(<td key={headings.length}>{key}</td>);
 			for (var i=0; i< headings.length; i++) {
-				values.push(<td key={i}>{this.state.rankings[row_num][headings[i]]}</td>);
+				values.push(<td key={i}>{this.state.rankings[key][headings[i]]}</td>);
+				//console.log("value" , values);
 			}
 			rows.push(<tr key={row_num}>{values}</tr>);
+			//console.log("rows", rows);
+			//break;
+			row_num++;
 		}
+
 		return (
             <div className="App">
             <header className="App-header">
